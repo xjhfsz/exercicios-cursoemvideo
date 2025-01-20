ec0b2f30ed9dc4db45c14e3dcc3f57f339722198
xjhfsz <jhfs632@gmail.com>
2025-01-19 22:11:53 -0300

    ex021

---
+++ exercicios/ex021.py
@@ -0,0 +1,15 @@
+import pygame
+import time
+
+pygame.init()  # inicia o pygame
+
+pygame.mixer.music.load('exercicios/ex021.mp3')  # carrega o arquivo mp3
+pygame.mixer.music.play()  # reproduz o arquivo mp3
+
+# versão windows
+pygame.event.wait()  # aguarda o fim da musica
+
+# # versão linux
+# while pygame.mixer.music.get_busy():  # aguarda o fim da música
+#     time.sleep(1)
+
